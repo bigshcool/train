dce151598066c0be183614821c02916b7568b93b
bigshcool <990194923@qq.com>
2023-08-03 11:21:56 +0800

    完成表名和实体名的获取

--- generator/src/main/java/com/rl/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/rl/train/generator/server/ServerGenerator.java
@@ -2,6 +2,7 @@ package com.rl.train.generator.server;
 
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -21,14 +22,25 @@ public class ServerGenerator {
 //        Map<String, Object> param = new HashMap<>();
 //        param.put("domain", "Test");
 //        FreemarkerUtil.generator(toPath + "Test.java", param);
+        String generatorPah = getGeneratorPah();
+        SAXReader saxReader = new SAXReader();
+        Document document = saxReader.read("generator/" + generatorPah);
+        Node table = document.selectSingleNode("//table");
+        Node tableName  = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    
+    }
+    
+    private static String getGeneratorPah() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         HashMap<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
-        System.out.println(node.getText());
+        return node.getText();
     }
-
-
+    
+    
 }
